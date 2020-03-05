b18637d8c932bd32c19b1ef7c754e5ff6123896d
yohaa0 <zhqj2008@163.com>
2020-03-05 23:08:19 +0800

    Signed-off-by: yohaa0 <zhqj2008@163.com>

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 #qpy:quiet
 #-*-coding:utf8;-*-
 """
-This is a sample project which use SL4A UI Framework,
-There is another Sample project: https://github.com/qpython-android/qpy-calcount
+This is a sample project 
+which use SL4A UI Framework
 """
 import qpy
 import androidhelper
@@ -46,47 +46,38 @@ class MainScreen(Layout):
 		android:layout_width="fill_parent"
 		android:layout_height="0px"
 		android:orientation="horizontal"
-		android:layout_weight="6">
-
-		<TextView
-			android:layout_width="fill_parent"
-			android:layout_height="fill_parent"
-			android:textSize="10dp"
-			android:text="帮你读书！"
-			android:textColor="#fffaff00"
-			android:layout_weight="1"
-			android:gravity="center"
-		/>
-		 </LinearLayout>
-	<LinearLayout
-		android:layout_width="fill_parent"
-		android:layout_height="0px"
-		android:orientation="horizontal"
-		android:layout_weight="50">
+		android:layout_weight="75">
 		
-		<TextView
-	 	android:id="@+id/text1"
-			android:layout_width="fill_parent"
-			android:layout_height="fill_parent"
-			android:textSize="8dp"
-			android:textColor="#ffffffff"
-			android:background="#ff05000f"
-			android:layout_weight="60"
-			android:gravity="left"
-		/>
+<ScrollView
+    android:id="@+id/sv_show"
+    android:fillViewport="true"
+    android:layout_width="match_parent"
+    android:layout_height="match_parent">
+    <TextView
+        android:id="@+id/text1"
+        android:layout_width="match_parent"
+        android:layout_height="match_parent"
+        android:hint="Display text here"
+        android:textSize="8dp"
+        android:scrollbars="vertical"
+        android:singleLine="false"
+        android:textColor="#ffffffff"
+        android:background="#ff0E4200"
+/>
+</ScrollView>
     </LinearLayout>
 
 	<ListView
 		android:id="@+id/data_list"
 		android:layout_width="fill_parent"
 		android:layout_height="0px"
-		android:layout_weight="25"
+		android:layout_weight="1"
 		/>
 	<LinearLayout
 		android:layout_width="fill_parent"
 		android:layout_height="0px"
 		android:orientation="horizontal"
-		android:layout_weight="8">
+		android:layout_weight="7">
 		<Button
 			android:layout_width="fill_parent"
 			android:layout_height="fill_parent"
@@ -124,42 +115,37 @@ class MainScreen(Layout):
         file_path=[]#文档路径
         for dirname,dirnames,filenames in os.walk('/storage/emulated/0/text'):
            for filename in filenames:
-               if os.path.splitext(filename)[1].lower()=='.txt':
+               if os.path.splitext(filename)[1].lower()=='.txt':#文档过滤
                   file_path.append(os.path.join(dirname,filename))
                   file_list.append(filename)
                #else:
                #   droid.makeToast('错误！没有文本文档，目前只支持文本文档')
                   
         print(file_path)
-        droid.dialogCreateAlert('/storage/emulated/0/text')
+        droid.dialogCreateAlert('/storage/emulated/0/text/')
         droid.dialogSetItems(file_list)
         droid.dialogSetPositiveButtonText('OK')        
         droid.dialogShow()
         #选择文档结果
         set_file=droid.dialogGetResponse().result
         print(file_list[set_file['item']])
-        droid.ttsSpeak('您选的是'+file_list[set_file['item']][:-4])
+        droid.ttsSpeak('您选的是'+file_list[set_file['item']][:-4])#-4去掉.txt
         #获取文档_名字_及全路径
         file_name =file_path[set_file['item']] 
-       # 打开文档
-        file_read = open(file_name, mode="r", encoding="utf-8") 
-     #一行一行读取文件内容到list 并去掉_行结束符
-        make_list = [l.strip() for l in file_read.readlines()] 
-       # 关闭文档
-        file_read.close()
-        return make_list
+        with open(file_name,"r+", encoding="utf-8")as obj:
+        #一行一行读取文件内容到list 并去掉_行结束符
+             make_list = [l.strip() for l in obj.readlines()]
+             print(make_list)
+        return make_list#返回文档每一句组成的list
       
     def load(self, view, dummy):
         droid = FullScreenWrapper2App.get_android_instance()
         droid.makeToast("请选择UTF-8文本格式小说！")
         droid.ttsSpeak("请选择UTF-8文本格式小说！")
-        saved_logo = qpy.tmp+"/qpy.logo"
-        ur.urlretrieve("https://www.qpython.org/static/img_logo.png", saved_logo)
-        self.views.logo.src = "file://"+saved_logo
-        #获取要读的文本文档每一行list
-           
+        self.views.logo.src = "file:///storage/emulated/0/qpython/projects3/ReadBook/读小说.png"
+        #定义要读的文本文档每一行list
         txt_list=[]
-        txt_list=MainScreen.select_file()
+        txt_list=MainScreen.select_file()#调取函数
         txt_list_len=len(txt_list)
         droid.ttsSpeak(('文章共有'+str(txt_list_len)+'行数, 从哪一段开始阅读?请输入行数字。'))
         get_message = droid.dialogGetInput('总共'+str(txt_list_len)+'行', '从哪一段开始阅读?').result 
@@ -169,31 +155,34 @@ class MainScreen(Layout):
         get_message2 = droid.dialogGetInput('还有'+str(txt_list_len-start_num)+"行", '您需要阅读多少行?').result
         end_num=int(get_message2)
         time.sleep(1)
-        read_num=0#结束计数
+        read_num=0#计数
+        dayintxt2=""
+        see_num=0
         if (start_num>txt_list_len):
            droid.makeToast('输入错误行数，请退出')
            droid.ttsSpeak('输入错误行数，请退出')
         else:
            for p in range(start_num,txt_list_len):
-               #Rdtext="%s"%(line)
-               #self.views.text1.text=""
                Rdtext=txt_list[p]
                read_num=read_num+1
+               see_num=see_num+1
                print(read_num,p,Rdtext)
                dayintxt=str(read_num)+"_"+str(p)+"_"+Rdtext
-               self.views.text1.text=dayintxt
+               dayintxt2=str(dayintxt2)+'\n'+dayintxt
+               self.views.text1.text=dayintxt2
                #droid.makeToast(str(read_num)+str(p)+Rdtext)
-               
-         #droid.setTtsPitch(2)
+               #droid.setTtsPitch(2)无效？
                droid.ttsSpeak(Rdtext)
                time.sleep(5)
                if end_num<=read_num:#到设定次数停止
                      self.views.text1.text="已读完，请退出！"
                      droid.ttsSpeak(self.views.text1.text)
                      break
+               elif see_num>=5:#显示五行归零
+                    dayintxt2=""
+                    see_num=0
                else:
                      time.sleep(1)
-         
         self.views.text1.text="已读完，请退出！"
         droid.ttsSpeak(self.views.text1.text)
             
